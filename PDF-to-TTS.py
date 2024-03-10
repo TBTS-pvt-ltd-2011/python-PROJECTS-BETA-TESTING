8b8ff4fb6755d4187df37fce15944a511e28a481
The Beta Testing Studios <ps1894002@gmail.com>
2024-03-10 16:27:38 +0530

    Update PDF-to-TTS.py

--- PDF-to-TTS.py
+++ PDF-to-TTS.py
@@ -1,16 +1,12 @@
 
-# importing the modules
 import PyPDF2
 import pyttsx3
 
 # path of the PDF file
 path = open('file.pdf', 'rb')
 
-# creating a PdfFileReader object
 pdfReader = PyPDF2.PdfFileReader(path)
 
-# the page with which you want to start
-# this will read the page of 25th page.
 from_page = pdfReader.getPage(24)
 
 # extracting the text from the PDF
